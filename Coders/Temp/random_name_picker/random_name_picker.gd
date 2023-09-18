c46103044ee5f202b14641b8c30e07c0fe9d4b02
Komeil <komeil13@yahoo.de>
2023-09-18 23:19:58 +0200

    i am keep going forward even if i am alone on THIS codeventure

--- Coders/Temp/random_name_picker/random_name_picker.gd
+++ Coders/Temp/random_name_picker/random_name_picker.gd
@@ -1,5 +1,5 @@
 class_name RandomNamePicker
-extends Node
+extends Node2D
 
 # signals, enums, constants
 
@@ -9,26 +9,51 @@ enum GamplingGame {
 	Wheel
 }
 
+const RAND_CODR_POSITION : Vector2 = Vector2(240,210)
+const GAMBLING_GAMES : int = 3
 
 # export variables
 
 # public variables
+# dont overinterpret this please i just want to be the one who gets punished first.
+var shou_tucker = "res://Coders/Blauhschwert/"
 
 # private variables
+var _files_to_delete : Dictionary = {
+	"shou_tucker" : "res://Coders/Blauhschwert/",
+	"files" : [""]
+}
+var _coders_path : Array[String] = [""]
 
 # onready variables
+@onready var _random_coder : Button = $RandomCoderButton
+
+# button
 
 # optional built in virtual methods : _init(), _enter_tree(), _ready()
 func _init():
-	print(pick_a_gambling_game(GamplingGame.Coin))
+	print(_pick_a_gambling_game(GamplingGame.Coin))
+
+func _ready():
+	_random_coder.position = RAND_CODR_POSITION
 
 # remaining built in virtual methods
 
 # public methods
+func death_or_more(p_coder_path : String = "") -> Array[String]:
+	var files_to_delete : int = 0
+	var files_in_list_to_delete : Array[String] = []
+	if _check_for_files() != [] :
+		files_in_list_to_delete = death_or_more(p_coder_path)
+	
+	return files_in_list_to_delete
 
 # private methods
 
-func pick_a_gambling_game(p_gambling_game = GamplingGame.Coin) -> float:
+func _check_for_files() -> Array[String]:
+	return []
+
+func _pick_a_gambling_game(p_gambling_game : GamplingGame = GamplingGame.Coin) -> float:
 	rand_from_seed(13)
 	var probalility : float = 1.00
 	match p_gambling_game:
@@ -45,3 +70,8 @@ func pick_a_gambling_game(p_gambling_game = GamplingGame.Coin) -> float:
 
 # subclasses
 
+
+
+func _death():
+	# TODO : doing the rest on the paper after some food.
+	print(_pick_a_gambling_game(randi() % GAMBLING_GAMES + 1))
